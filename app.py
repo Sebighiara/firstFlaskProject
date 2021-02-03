c9f2ba1de668662972f3a810441eea9437faf107
Sebighiara <sebi.gg98@gmail.com>
2021-02-03 03:36:02 +0200

    doing register form

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8ee409b7667f32241bec4b83cec5e938'
 
 posts = [
 	{
@@ -21,11 +24,16 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def homePage():
-	return render_template('index.html', posts = posts)
+	return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def aboutPage():
 	return render_template('about.html', title = 'about')
+
+@app.route('/register')
+def registerPage():
+	form = RegistrationForm()
+	return render_template('register.html', form = form)
 
 if __name__ == "__main__":
 	app.run(debug = True)
