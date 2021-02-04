334d491038e93d476645e0e1673edd0babb8e7cf
Sebighiara <sebi.gg98@gmail.com>
2021-02-04 04:23:54 +0200

    created subscribe page

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
 from flask import Flask, render_template, url_for
-from forms import RegistrationForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '8ee409b7667f32241bec4b83cec5e938'
-
 posts = [
 	{
-		"title": "blog title 1",
-		"content": "first blog content",
-		"author": "x",
-		"datePosted": "Monday, 1st, 2021"
+		"title": "Blog Post 1",
+		"datePosted": "Thursday, 4 February, 2021",
+		"author": "Sebi",
+		"content": "1st blog content"
 	},
 	{
-		"title": "blog title 2",
-		"content": "second blog content",
-		"author": "y",
-		"datePosted": "Monday, 1st, 2021"
-
+		"title": "Blog Post 2",
+		"datePosted": "Thursday, 4 February, 2021",
+		"author": "Mariana",
+		"content": "2nd blog content"
 	}
+
 ]
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def homePage():
-	return render_template('home.html', posts = posts)
+	title = 'home page'
+	return render_template('home.html', title = title)
 
-@app.route('/about')
+@app.route("/about")
 def aboutPage():
-	return render_template('about.html', title = 'about')
+	title = 'about page'
+	return render_template('about.html', title = title, posts = posts)
 
-@app.route('/register')
-def registerPage():
-	form = RegistrationForm()
-	return render_template('register.html', form = form)
+@app.route("/subscribe")
+def subscribePage():
+	title = 'Subscribe to my Email Newsletter'
+	return render_template('subscribe.html', title = title)
+
 
 if __name__ == "__main__":
 	app.run(debug = True)
--- forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-
-
-class RegistrationForm(FlaskForm):
-	username = StringField('Username')
