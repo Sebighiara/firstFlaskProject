417623961976d249e2f23c9028bbc294d5ca40ed
Sebighiara <sebi.gg98@gmail.com>
2021-02-05 03:03:58 +0200

    learned about databases

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"
+
+#initialize the database
+db = SQLAlchemy(app)
+
+#create database model
+class Friends(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(200), nullable = False) 
+	dateCreated = db.Column(db.DateTime, default = datetime.utcnow)
+	
+	#create a function to return a string when we add something
+	def __repr__(self):
+		return '<name %r>' % self.id 
 
 posts = [
 	{
@@ -18,6 +35,7 @@ posts = [
 
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def homePage():
@@ -34,6 +52,40 @@ def subscribePage():
 	title = 'Subscribe to my Email Newsletter'
 	return render_template('subscribe.html', title = title)
 
+@app.route("/form", methods = ["POST"])
+def formPage():
+	title = 'Thank you'
+	firstName = request.form.get("firstName")
+	lastName = request.form.get("lastName")
+	email = request.form.get("email")
+	if not firstName or not lastName or not email:
+		statementError = "All form fields required..."
+		return render_template("subscribe.html",
+			statementError = statementError, 
+			firstName = firstName,
+			lastName = lastName,
+			email = email)
+	return render_template('form.html', title = title, subscribers = subscribers)
+
+@app.route("/friends", methods = [ "GET", "POST"])
+def friendsPage():
+	title = "My friend list"
+	if request.method == "POST":
+		friendName = request.form["name"]
+
+		#add friend
+		newFriend = Friends(name = friendName)
+
+		#push to database
+		try:
+			db.session.add(newFriend)
+			db.session.commit()
+			return redirect(url_for('friendsPage'))
+		except:
+			return "There was an error adding your friend"
+	else:
+		friends = Friends.query.order_by(Friends.dateCreated)
+		return render_template('friends.html', title = title, friends = friends)
 
 if __name__ == "__main__":
 	app.run(debug = True)
